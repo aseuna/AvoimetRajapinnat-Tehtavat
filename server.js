b565df061665409598e6167f1300e0f12d4f54b3
aseuna <arttuseuna@gmail.com>
2020-10-18 03:27:07 +0300

    sm up

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(serveStatic(path.join(__dirname, 'build')));
 
 
-var server = app.listen(process.env.Port || 3000, function() {
+var server = app.listen(process.env.PORT|| 3000, function() {
     var port = server.address().port;
     console.log('Example app listening at port %s', port);
 });
